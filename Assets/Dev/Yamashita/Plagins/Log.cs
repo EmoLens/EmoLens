e7c4df10fe72da632b0cbf2d3f9d1ff6b6e35e01
Atsushi Yamashita <Atsushi Yamashita>
2017-10-14 19:34:24 +0900

    Add TestPrintTest and update project settings.

---
+++ Assets/Dev/Yamashita/Plagins/Log.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Runtime.CompilerServices;
+
+public static class Log
+{
+    public static void DebugThrow ( string message = "",
+        [CallerFilePath] string file = "",
+        [CallerLineNumber] int line = 0,
+        [CallerMemberName] string member = "" )
+    {
+        Debug.Log( $"{file}:{line}-{member}\n{message}" );
+    }
+}
