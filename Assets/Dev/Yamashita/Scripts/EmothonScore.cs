313a9cb93fb569bc6447e3cb78c298d03331ecb8
Atsushi Yamashita <Atsushi Yamashita>
2017-10-15 15:20:24 +0900

    Update Structure.

--- Assets/Dev/Yamashita/Scripts/EmothonScore.cs
+++ Assets/Dev/Yamashita/Scripts/EmothonScore.cs
@@ -1,14 +1,36 @@
-﻿public class EmotionScore
+﻿using AY_Util;
+using System.Collections.Generic;
+
+public class EmotionScore
 {
-    public float[] value = new float[( int )VALUE.SIZE];
+    private float[] mValue = new float[( int )VALUE.SIZE];
 
     public enum VALUE
     {
-        ANGER, CONTEMPT, DISGUST, FEAR, HAPPINESS, NATURAL, SADNESS, SURPRISE, SIZE
+        anger, contempt, disgust, fear, happiness, neutral, sadness, surprise, SIZE
+    }
+
+    public static KeyValuePair<VALUE, string>[] Subjects ( )
+    {
+        KeyValuePair<VALUE, string>[] arr = new KeyValuePair<VALUE, string>[( int )VALUE.SIZE];
+
+        for (int i = 0; i < ( int )VALUE.SIZE; i++)
+        {
+            arr[i] = new KeyValuePair<VALUE, string>(
+                EnumUtil<VALUE>.GetElement( i ),
+                EnumUtil<VALUE>.GetString( i ) );
+        }
+        return arr;
     }
 
     public float Get ( VALUE key )
     {
-        return value[( int )key];
+        return mValue[( int )key];
+    }
+
+    public EmotionScore Set ( VALUE key, double value )
+    {
+        mValue[( int )key] = ( float )value;
+        return this;
     }
 }
--- Assets/Dev/Yamashita/Scripts/FaceRectangle.cs
+++ Assets/Dev/Yamashita/Scripts/FaceRectangle.cs
@@ -1,14 +1,37 @@
-﻿public class FaceRectangle
+﻿using System.Collections.Generic;
+using AY_Util;
+
+public class FaceRectangle
 {
-    public float[] value = new float[( int )VALUE.SIZE];
+    private float[] mValue = new float[( int )VALUE.SIZE];
 
     public enum VALUE
     {
-        TOP, LEFT, WIDTH, HEIGHT, SIZE
+        top, left, width, height, SIZE
     }
 
     public float Get ( VALUE key )
     {
-        return value[( int )key];
+        return mValue[( int )key];
     }
+
+    public FaceRectangle Set ( VALUE key, double value )
+    {
+        mValue[( int )key] = ( float )value;
+        return this;
+    }
+
+    public static KeyValuePair<VALUE,string>[] Subjects ( )
+    {
+        KeyValuePair<VALUE, string>[] arr = new KeyValuePair<VALUE, string>[(int)VALUE.SIZE];
+        for (int i = 0; i < ( int )VALUE.SIZE; i++)
+        {
+            arr[i] = new KeyValuePair<VALUE, string>(
+                EnumUtil<VALUE>.GetElement( i ),
+                EnumUtil<VALUE>.GetString( i ) );
+        }
+
+        return arr;
+    }
+
 }
